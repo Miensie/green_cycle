b54b77068a586546cc5b6e6548f0ce4882d32d8c
Miensie <koffimiensie@gmail.com>
2025-09-14 23:02:48 +0000

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Dashboard from './dashboard/Dashboard.jsx'
-import About from './about/About.jsx'
-import Contact from './contact/Contact1.jsx'
-import Activity from './activity/Activity.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: < Dashboard/>,
-  },
-  {
-    path: "/about",
-    element: < About />
-  },
-  {
-    path: "/contact",
-    element: <Contact />
-  },
-  {
-    path: "/activity",
-    element: <Activity />
-  }
-
-
-])
-
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-     <RouterProvider 
-     router={router}>
-        
-     </RouterProvider>
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
